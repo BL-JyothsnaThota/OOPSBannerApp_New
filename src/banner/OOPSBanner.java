c3becad24ce9d9b9a377b8b48231799be4ca257c
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-01-29 16:30:29 +0530

    Render OOPS as Banner using Print Statements

---
+++ src/banner/OOPSBanner.java
@@ -0,0 +1,14 @@
+package banner;
+
+public class OOPSBanner {
+
+	public static void main(String[]args){
+		System.out.println("  *****   "+"  *****   "+"********  "+"********* ");
+		System.out.println(" *     *  "+" *     *  "+"*       * "+"*         ");
+		System.out.println("*       * "+"*       * "+"*       * "+"*         ");
+		System.out.println("*       * "+"*       * "+"********  "+"********* ");
+		System.out.println("*       * "+"*       * "+"*         "+"        * ");
+		System.out.println(" *     *  "+" *     *  "+"*         "+"        * ");
+		System.out.println("  *****   "+"  *****   "+"*         "+"********* ");
+	}
+}
